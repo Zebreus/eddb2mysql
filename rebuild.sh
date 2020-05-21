94e06694f195493b54c55e1d1230fd17c17fde52
Zebreus <lennart@madmanfred.com>
2020-05-21 19:50:56 +0200

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -35,13 +35,14 @@ fi
 if [ ! -f ${DATADIR}/modules.csv ]; then
 	wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v6/modules.json | gzip -dc | json2csv/bin/json2csv.js -f "id","group_id","class","rating","price","weapon_mode","missile_type","name","belongs_to","ed_id","ed_symbol","ship" | tail -n +2 > ${DATADIR}/modules.csv
 fi
+
 # Missing fields on stations:
-# "import_commodities","export_commodities","prohibited_commodities","economies","selling_ships","selling_modules"
+# "import_commodities","export_commodities","prohibited_commodities","economies","selling_ships","selling_modules", "states"
 if [ ! -f ${DATADIR}/stations_import.csv ]; then
-	wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v6/stations.json | gzip -dc | json2csv/bin/json2csv.js -f "id","name","system_id","updated_at","max_landing_pad_size","distance_to_star","government_id","allegiance_id","state_id","type_id","type","has_blackmarket","has_market","has_refuel","has_repair","has_rearm","has_outfitting","has_shipyard","has_docking","has_commodities","shipyard_updated_at","outfitting_updated_at","market_updated_at","is_planetary","settlement_size_id","settlement_size","settlement_security_id","settlement_security","body_id","controlling_minor_faction_id" | tail -n +2 > ${DATADIR}/stations_import.csv
+	wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v6/stations.json | gzip -dc | json2csv/bin/json2csv.js -f "id","name","system_id","updated_at","max_landing_pad_size","distance_to_star","government_id","allegiance_id","type_id","type","has_blackmarket","has_market","has_refuel","has_repair","has_rearm","has_outfitting","has_shipyard","has_docking","has_commodities","shipyard_updated_at","outfitting_updated_at","market_updated_at","is_planetary","settlement_size_id","settlement_size","settlement_security_id","settlement_security","body_id","controlling_minor_faction_id" | tail -n +2 > ${DATADIR}/stations_import.csv
 fi
 # Missing fields in bodies:
-# "discovery","parents","belts"
+# "discovery","parents","belts", "states"
 if [ ! -f ${DATADIR}/bodies.csv ]; then
 	wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v6/bodies_recently.jsonl | gzip -dc | json2csv/bin/json2csv.js -f "id","bodyId","name","type","subType","offset","distanceToArrival","isMainStar","isScoopable","age","spectralClass","luminosity","absoluteMagnitude","solarMasses","solarRadius","surfaceTemperature","orbitalPeriod","semiMajorAxis","orbitalEccentricity","orbitalInclination","argOfPeriapsis","rotationalPeriod","rotationalPeriodTidallyLocked","axialTilt","updateTime","systemId" | tail -n +2 | split -d -l 10000000 - ${DATADIR}/bodies.csv
 	touch ${DATADIR}/bodies.csv
@@ -51,7 +52,7 @@ echo "Loading data into MySQL"
 mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost $mysqldb ${DATADIR}/systems_import.csv*
 mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost $mysqldb ${DATADIR}/stations_import.csv
 # Direct table loads
-mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,name,updated_at,government_id,allegiance_id,state_id,home_system_id,is_player_faction $mysqldb ${DATADIR}/factions.csv
+mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,name,updated_at,government_id,government,allegiance_id,home_system_id,is_player_faction $mysqldb ${DATADIR}/factions.csv
 mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,station_id,commodity_id,supply,supply_bracket,buy_price,sell_price,demand,demand_bracket,collected_at $mysqldb ${DATADIR}/listings.csv
 mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,name,category_id,average_price,is_rare,max_buy_price,max_sell_price,min_buy_price,min_sell_price,buy_price_lower_average,sell_price_upper_average,is_non_marketable,ed_id $mysqldb ${DATADIR}/commodities.csv
 mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\n' --ignore -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,group_id,class,rating,price,weapon_mode,missile_type,name,belongs_to,ed_id,ed_symbol,ship $mysqldb ${DATADIR}/modules.csv
